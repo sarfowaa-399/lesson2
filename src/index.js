04c67a102ba8678a46a303f9648635d026e14d3c
Your Name <amasarfowaap@gmail.com>
2024-12-13 09:34:57 +0000

    added all files and images

---
+++ src/index.js
@@ -0,0 +1,13 @@
+console.log(5);
+
+let nam1 =1;
+console.log("nam1")
+
+let nam2 =2; 
+console.log("nam2")
+
+let nam3 =3;
+console.log("nam3");
+
+let job = ("mining");
+console.log(`my dream job is ${job}`);
